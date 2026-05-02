a25963252e600a162638277577e9f4a67707a570
Marcos Rivera <mjrivera@knowmadmood.com>
2026-05-02 18:07:53 +0200

    feat: add product suggestions component and API endpoint

---
+++ app/components/product/Suggestions.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  slug: string;
+}>();
+
+const { data: productSuggestions, status } = useFetch(
+  `/api/product/${props.slug}/suggestions`,
+  {
+    lazy: true,
+    server: false,
+    cache: 'force-cache',
+  }
+);
+</script>
+<template>
+  <div>
+    <div
+      v-if="status === 'pending'"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+    >
+      <USkeleton class="w-full h-52 rounded-md mb-4" />
+      <USkeleton class="w-full h-52 rounded-md mb-4" />
+      <USkeleton class="w-full h-52 rounded-md mb-4" />
+    </div>
+
+    <ProductsGrid v-else :products="productSuggestions || []" />
+  </div>
+</template>
